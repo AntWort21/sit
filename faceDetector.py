f12f142cb0c4600fbd5c7f2928cd9ea53f63c059
aditya <adityawinata21@gmail.com>
2022-05-17 15:00:36 +0900

    removed video file, added found/notFound flag

--- faceDetector.py
+++ faceDetector.py
@@ -14,7 +14,11 @@ capture = cv2.VideoCapture(0)
 #     (640,480)
 # )
 
+found = 0
+notFound = 0
+
 while(True):
+
     ret, frame = capture.read()
 
     frame = cv2.resize(frame, (640, 480))
@@ -22,15 +26,24 @@ while(True):
 
     detected_face = face_cascade.detectMultiScale(gray, scaleFactor=1.05, minNeighbors=5)
     
-    #maybe put it here?
-    for x, y, w, h in detected_face:
-        cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+    if(len(detected_face) > 0):
+        for x, y, w, h in detected_face:
+            cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+            found+=1
+            notFound = 0 #if face is detected, reset notFound
+            if found == 80:
+                print("Occupied") #send the data somewhere, then break the program
+    else:
+        notFound += 1
+        found = 0 #if face is not detected, reset found
+        if notFound == 80:
+            print("Vacant")
+    
         # time.sleep(10)
         # print("output")
         #output 1 and 0 where face is detected and then save it into a notepad file
         #the result from notepad connect it to the android application
     
-    # time.sleep(10) #works like thread.sleep() in java programming
 
     # out.write(frame.astype('uint8'))
     cv2.imshow('frame', frame)
